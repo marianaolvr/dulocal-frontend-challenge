a5d2ca0654298ba908dabde58f0df264d59ac753
Mariana Oliveira <olvr.mariana@gmail.com>
2020-07-28 22:32:39 -0300

    add login page content

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,6 +4,18 @@ import './styles.css';
 
 export default function Login() {
     return (
-        <h1>Login works (:</h1>
+        <div class="login-container">
+            <h1>Bem-vindo de volta!</h1>
+            <section className="form">
+                <form action="">
+                    <h2>Faça seu login:</h2>
+                    <input type="email" name="" id="" placeholder="E-mail" />
+                    <input type="password" name="" id="" placeholder="Senha" />
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">Não tenho cadastro</a>
+                </form>
+            </section>
+        </div>
     );
 };
