d4abece7b217be30530339c758b46a2bd5083927
Mariana Oliveira <olvr.mariana@gmail.com>
2020-07-28 23:52:47 -0300

    change class to classname and add hover for button

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,14 +4,14 @@ import './styles.css';
 
 export default function Login() {
     return (
-        <div class="login-container">
+        <div className="login-container">
             <h1>BEM-VINDO DE VOLTA!</h1>
             <section className="login-form">
                 <form action="">
                     <h2>Faça seu login:</h2>
                     <input type="email" name="" id="" placeholder="E-mail" />
                     <input type="password" name="" id="" placeholder="Senha" />
-                    <button type="submit" class="button">Entrar</button>
+                    <button type="submit" className="button">Entrar</button>
 
                     <a href="/register">Não tenho cadastro</a>
                 </form>
