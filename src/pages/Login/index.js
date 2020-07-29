9f034cd4ab76b1c376035e1289f72fd9dcca0958
Mariana Oliveira <olvr.mariana@gmail.com>
2020-07-28 23:15:25 -0300

    adds button global style and organizes all created css

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,13 +5,13 @@ import './styles.css';
 export default function Login() {
     return (
         <div class="login-container">
-            <h1>Bem-vindo de volta!</h1>
+            <h1>BEM-VINDO DE VOLTA!</h1>
             <section className="login-form">
                 <form action="">
                     <h2>Faça seu login:</h2>
                     <input type="email" name="" id="" placeholder="E-mail" />
                     <input type="password" name="" id="" placeholder="Senha" />
-                    <button type="submit">Entrar</button>
+                    <button type="submit" class="button">Entrar</button>
 
                     <a href="/register">Não tenho cadastro</a>
                 </form>
